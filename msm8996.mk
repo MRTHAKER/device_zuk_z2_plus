f72156fd25483760600b8027f9da6277f058474c
dianlujitao <dianlujitao@lineageos.org>
2017-08-13 22:36:42 +0800

    z2_plus: Kang marlin thermal config file
    Change-Id: I6a5d4c8327cdfdcb62647421b8506dcbd303a204

--- msm8996.mk
+++ msm8996.mk
@@ -263,6 +263,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp_policy/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
+
 # Wifi
 PRODUCT_PACKAGES += \
     libwpa_client \
