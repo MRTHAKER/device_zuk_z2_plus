003c526a7fdc29d6e2bea07df6f2528b8af43709
Ankur Maheshwari <mankur@codeaurora.org>
2017-09-13 18:53:39 +0530

    z2_plus: HAL: Power Hint not working
    Issue:
    Power Hint settings are not applied to video
    preview screen in camera after recording done.
    
    Fix:
    No need to apply Power hint settings in recording
    because the Power hint applied in preview will be
    there in recording as well.
    
    Change-Id: Ibafa879eb75c6152517abbef2b5ff1d61548878d

--- camera/QCamera2/HAL/QCamera2HWI.cpp
+++ camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -3700,11 +3700,6 @@ int QCamera2HardwareInterface::startRecording()
         rc = pChannel->start();
     }
 
-    if (rc == NO_ERROR) {
-        // Set power Hint for video encoding
-        m_perfLock.powerHint(POWER_HINT_VIDEO_ENCODE, true);
-    }
-
     LOGI("X rc = %d", rc);
     return rc;
 }
@@ -3731,8 +3726,6 @@ int QCamera2HardwareInterface::stopRecording()
     int rc = stopChannel(QCAMERA_CH_TYPE_VIDEO);
 
     m_cbNotifier.flushVideoNotifications();
-    // Disable power hint for video encoding
-    m_perfLock.powerHint(POWER_HINT_VIDEO_ENCODE, false);
     LOGI("X rc = %d", rc);
     return rc;
 }
