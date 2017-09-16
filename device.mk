cf7428a26bd8a9fbf10dc8275812e9b6aa3ee058
Álvaro Brey <alvaro.brv@gmail.com>
2017-09-16 17:24:52 +0200

    msm8996-common: rootdir: Kang init.qcom.usb.rc from gemini
     * Cleaner, and includes stuff from init.qcom.usb.sh without needing the script
    
    msm8996-common: rootdir: Import back USB compositions from the old .rc
    
     * Fixes ADB and MTP on z2_plus

--- device.mk
+++ device.mk
@@ -288,7 +288,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.sh \
     init.qcom.usb.rc \
-    init.qcom.usb.sh \
     init.qcom.bt.sh \
     ueventd.qcom.rc \
     init.qcom.sensors.sh
