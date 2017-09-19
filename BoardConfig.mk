d4a12aa1c90e582d8e88ea2ae783476bf519b235
dd3boh <dade.garberi@gmail.com>
2017-09-19 14:29:43 +0200

    ril: Build caf and use a in-device data service
    Signed-off-by: dd3boh <dade.garberi@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,10 @@ TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Ril
+TARGET_RIL_VARIANT := caf
+USE_DEVICE_SPECIFIC_DATASERVICES := true
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
