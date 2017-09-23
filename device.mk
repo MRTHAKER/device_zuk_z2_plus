0f71782e5b1a392408c127e4295b71294bc5ef31
davide <davidevinavil@gmail.com>
2017-09-23 23:46:09 +0200

    audio: Import audio_effects.conf

--- device.mk
+++ device.mk
@@ -95,6 +95,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
