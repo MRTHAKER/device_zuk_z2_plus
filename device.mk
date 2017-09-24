38304ecd59f555bd5f2458cde6f93b831e3bc440
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-24 02:32:53 +0200

    HIDL: build all the HALs

--- device.mk
+++ device.mk
@@ -159,6 +159,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libsdm-disp-apis
 
+# HIDL
+$(call inherit-product, $(LOCAL_PATH)/hidl.mk)
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
