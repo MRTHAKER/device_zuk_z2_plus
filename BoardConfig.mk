622a275bc81f3ef6feaa1a74e885a3c5c7dbc5c2
Martinusbe <martinusbe@gmail.com>
2017-09-25 02:33:38 +0200

    sepolicy: Import from Marlin

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,7 +190,7 @@ USE_DEVICE_SPECIFIC_DATASERVICES := true
 
 # SELinux
 # include device/qcom/sepolicy/sepolicy.mk
-# BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Tap to wake 
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/touch/tp_dev/gesture_on"
