12051c90e8ec8d13853ab431636a89724250c752
davide <davidevinavil@gmail.com>
2017-09-30 16:21:28 +0200

    device.mk: Remove duplicate package

--- device.mk
+++ device.mk
@@ -129,10 +129,6 @@ PRODUCT_PACKAGES += \
 # PRODUCT_PACKAGES += \
 #     ZukDoze
 
-# Dumpstate HAL
-PRODUCT_PACKAGES += \
-    android.hardware.dumpstate@1.0-service.msm8996
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     fingerprint.msm8996
