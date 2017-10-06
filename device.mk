3f2c8e7c2490987db8f3416d4c0591e84ef93918
MRTHAKER <mahek.haker@gmail.com>
2017-10-06 17:42:07 +0000

    z2_plus:update thermal config

--- device.mk
+++ device.mk
@@ -226,6 +226,8 @@ PRODUCT_PACKAGES += \
     sensors.msm8996
 
 # Thermal
+PRODUCT_PACKAGES += thermal.msm8996
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
 
