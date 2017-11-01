e90c78bf7f0674c8aec03a2f4222fb7417d99458
davide <davidevinavil@gmail.com>
2017-11-01 21:17:52 +0100

    camera: Switch to stock aosp camera implementation

--- hidl.mk
+++ hidl.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl.legacy
+    android.hardware.camera.provider@2.4-impl
 
 # Display
 PRODUCT_PACKAGES += \
