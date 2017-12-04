3806cd51bafb5947d0263ba530b2e5ab3b9fbf6f
MRTHAKER <mahek.haker@gmail.com>
2017-12-04 19:54:42 +0000

    z2_plus:build vr hal

--- hidl.mk
+++ hidl.mk
@@ -94,3 +94,8 @@ PRODUCT_PACKAGES += \
 # WiFi HAL
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
+
+#VR
+PRODUCT_PACKAGES += \
+    android.hardware.vr@1.0-impl \
+    android.hardware.vr@1.0-service
