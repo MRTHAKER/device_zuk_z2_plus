LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: Doze: Hide features if unsupported
 * This is required for lithium because the device has no ability to check
   for proximity sensor status during standby due to it's native design

Change-Id: I5d3ecfe3df96b5e58ffb0579dffd311c7c13e4a9

doze/src/com/cyanogenmod/settings/doze/Utils.java
@@ -19,6 +19,7 @@ package com.cyanogenmod.settings.doze;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.UserHandle;
 import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
@@ -32,6 +33,8 @@ public final class Utils {
     private static final boolean DEBUG = false;
 
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
+
+    protected static final String CATEGORY_PROXIMITY_SENSOR = "proximity_sensor";
 
     protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
@@ -54,6 +57,17 @@ public final class Utils {
             startService(context);
         } else {
             stopService(context);
+        }
+    }
+
+    protected static boolean getProxCheckBeforePulse(Context context) {
+        try {
+            Context con = context.createPackageContext("com.android.systemui", 0);
+            int id = con.getResources().getIdentifier("doze_proximity_check_before_pulse",
+                    "bool", "com.android.systemui");
+            return con.getResources().getBoolean(id);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
         }
     }
 

MRTHAKER/device_zuk_z2_plus

   for proximity sensor status during standby, due to sensor's native design
Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

doze/src/com/cyanogenmod/settings/doze/Utils.java
@@ -19,6 +19,7 @@ package com.cyanogenmod.settings.doze;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.UserHandle;
 import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
@@ -32,6 +33,8 @@ public final class Utils {
     private static final boolean DEBUG = false;
 
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
+
+    protected static final String CATEG_PROX_SENSOR = "proximity_sensor";
 
     protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
@@ -54,6 +57,17 @@ public final class Utils {
             startService(context);
         } else {
             stopService(context);
+        }
+    }
+
+    protected static boolean getProxCheckBeforePulse(Context context) {
+        try {
+            Context con = context.createPackageContext("com.android.systemui", 0);
+            int id = con.getResources().getIdentifier("doze_proximity_check_before_pulse",
+                    "bool", "com.android.systemui");
+            return con.getResources().getBoolean(id);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
         }
     }
 