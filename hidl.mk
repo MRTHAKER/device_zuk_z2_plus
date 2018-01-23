0b60bb11d77c571c4107c3337b73a6193770c3cd
mrthaker <mahek.haker@gmail.com>
2018-01-23 10:55:21 +0000

    Revert "zuk: Switch to fully binderized power hal"
    This reverts commit 73a7aaf07d5a391073a46b2b550fdbe08bd851ae.

--- hidl.mk
+++ hidl.mk
@@ -74,7 +74,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
