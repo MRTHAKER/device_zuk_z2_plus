59e1565afd3f45821bdaf6d52281f23ca6084d10
dianlujitao <dianlujitao@lineageos.org>
2018-01-25 11:14:21 +0800

    op3: Add OEM privileged app permissions
    Change-Id: If5425295a67449685737ab4e8099a5e2b432c78e

--- device.mk
+++ device.mk
@@ -247,6 +247,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vr.high_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vr.high_performance.xml
 
+# Qualcomm
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
