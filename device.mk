37344ce8522c3dbbaeb10d0aa4fe57b90a1e0219
mrthaker <mahek.haker@gmail.com>
2018-02-07 13:02:31 +0000

    move vr permission to vendor

--- device.mk
+++ device.mk
@@ -282,7 +282,7 @@ PRODUCT_PACKAGES += \
     vr.msm8996 \
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:system/etc/permissions/android.hardware.vr.high_performance.xml
+    frameworks/native/data/etc/android.hardware.vr.high_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vr.high_performance.xml
 
 # Wifi
 PRODUCT_PACKAGES += \
