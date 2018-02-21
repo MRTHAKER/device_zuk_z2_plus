c576af94cbb45b631b304febaf40310426afa579
MRTHAKER <mahek.haker@gmail.com>
2018-02-22 05:14:22 +0530

    Zuk: Build Gnss Hal

--- hidl.mk
+++ hidl.mk
@@ -62,8 +62,7 @@ PRODUCT_PACKAGES += \
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
-   android.hardware.gnss@1.0-impl-qti \
-   android.hardware.gnss@1.0-service-qti
+   android.hardware.gnss@1.0-impl
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
