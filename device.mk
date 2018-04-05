f9580ed561e4490d134a6bbd11ef35224bb5979d
MRTHAKER <mahek.haker@gmail.com>
2018-04-05 18:27:31 +0530

    Zuk: switch to new thermal service

--- device.mk
+++ device.mk
@@ -199,12 +199,10 @@ PRODUCT_PACKAGES += \
     ims-ext-common
 
 # Thermal
-PRODUCT_PACKAGES += \
-    thermal.msm8996
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal-engine-vr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-vr.conf
+
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
