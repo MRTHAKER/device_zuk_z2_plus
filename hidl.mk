255beedd9c6e733eb7f7ac064466cf43ebf1cc6a
MRTHAKER <mahek.haker@gmail.com>
2018-04-09 08:25:04 +0530

    Zuk: binderize some display/graphics Hals

--- hidl.mk
+++ hidl.mk
@@ -38,9 +38,20 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.configstore@1.0-service
+
+PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    vendor.display.color@1.0-impl \
+    vendor.display.color@1.0-service \
+    vendor.display.config@1.1 \
+    vendor.display.config@1.1_vendor \
 
 # DRM
 PRODUCT_PACKAGES += \
