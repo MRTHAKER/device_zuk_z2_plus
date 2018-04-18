428c8e9040b11ea8563563a90ad41ec2b7cc4634
MRTHAKER <mahek.haker@gmail.com>
2018-04-19 02:10:55 +0530

    Zuk: fix slowness of Camera

--- hidl.mk
+++ hidl.mk
@@ -32,7 +32,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+     camera.device@3.2-impl
 
 # Display
 PRODUCT_PACKAGES += \
