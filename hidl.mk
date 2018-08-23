804f3332a30fdbd7f59b063f23c967b374c44152
MRTHAKER <mahek.haker@gmail.com>
2018-08-23 11:40:47 +0200

    Zuk: Add custom biometric hal From AOSP -inspired from mido godix hal -also includes 2.0-->2.1 wrapper code

--- hidl.mk
+++ hidl.mk
@@ -50,7 +50,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.0-service.zuk_z2
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
