ae8c6aee910a42515e1b6bd303d6abf752221b53
MRTHAKER <mahek.haker@gmail.com>
2018-10-06 08:38:36 +0000

    Zuk: Remove custom provider

--- hidl.mk
+++ hidl.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service.zuk_z2
+    android.hardware.camera.provider@2.4-service
 
 #Display
 PRODUCT_PACKAGES += \
