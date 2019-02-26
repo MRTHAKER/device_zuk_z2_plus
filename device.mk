d69e4821dd04f7e8b0d73af129d57592827176ba
root <root@instance-1.asia-south1-c.c.true-source-223007.internal>
2019-02-26 11:51:19 +0000

    Pa Bringup

--- device.mk
+++ device.mk
@@ -71,12 +71,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# HWUI overrides
-$(call inherit-product, vendor/derp/build/configs/phone-xxxhdpi-3072-hwui-memory.mk)
-
-# Dalvik overrides
-$(call inherit-product, vendor/derp/build/configs/phone-xxxhdpi-3072-dalvik-heap.mk)
-
 # facelock properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.black_timeout=700 \
