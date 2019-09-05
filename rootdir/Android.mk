f0802553b475003334f3eac0b743cb5c91fb302a
Han Wang <416810799@qq.com>
2019-09-05 07:07:51 +0200

    z2_plus: Remove LOCAL_MODULE_TAGS := eng usage
    Change-Id: I83aa494618d9c4809d3219aa3785e56b48ef8dfb

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.qcom
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/fstab.qcom
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -12,7 +12,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.power.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.power.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -20,7 +20,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -28,7 +28,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.sh
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.sh
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -36,7 +36,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.early_boot.sh
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.early_boot.sh
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -44,7 +44,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -52,7 +52,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.qcom.rc
-LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
@@ -60,7 +60,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       	:= init.qcom.sensors.sh
-LOCAL_MODULE_TAGS 	:= optional eng
+LOCAL_MODULE_TAGS 	:= optional
 LOCAL_MODULE_CLASS 	:= ETC
 LOCAL_SRC_FILES    	:= etc/init.qcom.sensors.sh
 LOCAL_MODULE_PATH  	:= $(TARGET_ROOT_OUT)
