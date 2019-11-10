c2fd980229517777b736edd55a934bbae4402b21
Henrique Pereira <hlcpereira@pixelexperience.org>
2019-11-10 16:06:54 -0200

    z2_plus: Switch to cryptfshw QSEECom impl
    Change-Id: I029b633ac4b39f9f9e6f5bac4ace775668ff82f0

--- device.mk
+++ device.mk
@@ -190,6 +190,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # HIDL
 $(call inherit-product, $(LOCAL_PATH)/hidl.mk)
 
