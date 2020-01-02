6498d9e1ddd523c457ef56550dc1c6959db811df
MRTHAKER <mahek.haker@gmail.com>
2020-01-02 16:50:09 +0530

    z2_plus: Fix lights log spam

--- hidl.mk
+++ hidl.mk
@@ -85,7 +85,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Power
 PRODUCT_PACKAGES += \
